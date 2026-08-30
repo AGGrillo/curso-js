86b79b85373f6c322a2e9c6e7f4a11e45705ba6a
AGGrillo <alex43anosdeidade@gmail.com>
2026-08-30 20:07:42 -0300

    c

--- aula-12-ex/ex014/script.js
+++ aula-12-ex/ex014/script.js
@@ -1,20 +1,29 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
+    var msg1 = window.document.getElementById('msg1')
+    var msg2 = window.document.getElementById('msg2')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var horas = data.getHours()
     var minutos = data.getMinutes()
-    msg.innerHTML = `Agora são ${horas}:${minutos}.`
+    if (minutos < 10) {
+        msg2.innerHTML = `Agora são ${horas}:0${minutos}.`
+    } else {
+        msg2.innerHTML = `Agora são ${horas}:${minutos}.`
+    }
     if (horas >= 0 && horas < 6) {
+        msg1.innerHTML = 'BOM MADRUGADA!!'
         img.src = 'foto-madrugada.jpg'
         document.body.style.background = 'black'
     } else if (horas >= 6 && horas <= 11) {
+        msg1.innerHTML = 'BOM DIA!!'
         img.src = 'foto-manha.jpg'
         document.body.style.background = 'orange'
     } else if (horas > 11 && horas <= 17) {
+        msg1.innerHTML = 'BOM TARDE!!'
         img.src = 'foto-tarde.jpg'
         document.body.style.background = 'blue'
     } else {
+        msg1.innerHTML = 'BOM NOITE!!'
         img.src = 'foto-noite.jpg'
         document.body.style.background = 'darkblue'
     }

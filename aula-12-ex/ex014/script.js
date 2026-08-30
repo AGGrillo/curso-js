0ba11a83bd75b3cfb8b7c8b9087ff91110329ac3
AGGrillo <alex43anosdeidade@gmail.com>
2026-08-30 19:44:11 -0300

    c

--- aula-12-ex/ex014/script.js
+++ aula-12-ex/ex014/script.js
@@ -5,7 +5,10 @@ function carregar() {
     var horas = data.getHours()
     var minutos = data.getMinutes()
     msg.innerHTML = `Agora são ${horas}:${minutos}.`
-    if (horas >= 0 && horas <= 11) {
+    if (horas >= 0 && horas < 6) {
+        img.src = 'foto-madrugada.jpg'
+        document.body.style.background = 'black'
+    } else if (horas >= 6 && horas <= 11) {
         img.src = 'foto-manha.jpg'
         document.body.style.background = 'orange'
     } else if (horas > 11 && horas <= 17) {

e2ff16c66c999aeb8b0d04244efe0829fa3eb325
AGGrillo <alex43anosdeidade@gmail.com>
2026-08-30 19:20:40 -0300

    c

---
+++ aula-12-ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var horas = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${horas}:${minutos}.`
+    if (horas >= 0 && horas <= 11) {
+        img.src = 'foto-manha.jpg'
+        document.body.style.background = 'orange'
+    } else if (horas > 11 && horas <= 17) {
+        img.src = 'foto-tarde.jpg'
+        document.body.style.background = 'blue'
+    } else {
+        img.src = 'foto-noite.jpg'
+        document.body.style.background = 'darkblue'
+    }
+}
+
+carregar()

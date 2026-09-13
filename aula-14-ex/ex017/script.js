5b35d982d68d5aa24eefe841a79553faa2d633b9
AGGrillo <alex43anosdeidade@gmail.com>
2026-09-13 17:26:38 -0300

    c

---
+++ aula-14-ex/ex017/script.js
@@ -0,0 +1,21 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    tab.innerHTML = ''
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        let item = document.createElement('option')
+        item.text = 'Digite um número...'
+        tab.appendChild(item)
+    } else {
+        let n = Number(num.value)
+        let cont = 1
+        while (cont <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${cont} = ${n*cont}`
+            item.value = `tab${cont}`
+            tab.appendChild(item)
+            cont++
+        }
+    }
+}

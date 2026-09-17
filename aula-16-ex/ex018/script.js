7a26a04a7cea162003b65494fcc98a076f6fd0ad
AGGrillo <alex43anosdeidade@gmail.com>
2026-09-17 17:41:23 -0300

    c

--- aula-16-ex/ex018/script.js
+++ aula-16-ex/ex018/script.js
@@ -11,8 +11,8 @@ function eNumero(n) {
     }
 }
 
-function emLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
+function emLista(n, v) {
+    if (v.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
@@ -20,14 +20,43 @@ function emLista(n, l) {
 }
 
 function adicionar() {
-    if (eNumero(num.value) && emLista(num.value)) {
+    if (eNumero(num.value) && !emLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        result.innerHTML = ''
     } else {
         window.alert('Número inválido ou já encontrado na lista!')
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            } 
+        }
+        media = soma / tot
+        result.innerHTML = ''
+        result.innerHTML += `<p>Ao todo, temos ${tot} valores cadastrados.</p>`
+        result.innerHTML += `<p>O maior valor cadastrado foi ${maior}.</p?`
+        result.innerHTML += `<p>O menor valor cadastrado foi ${menor}.</p?`
+        result.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        result.innerHTML += `<p>A média dos valores cadastrados é ${media}.</p>`
+    }
 }
